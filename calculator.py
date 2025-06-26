0e9014903f9baa52b4ede5d84d4399b3cb759867
Omid Panahi <Omid.panahi@vaniercollege.qc.ca>
2025-06-26 11:21:48 -0400

    add cal

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def divide_numbers(a, b):
+    return a / b
+
+def main():
+    print("Welcome to the calculator")
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+
+    result = divide_numbers(num1, num2)
+    print("The result is:", result)
+
+if __name__ == "__main__":
+    main()
